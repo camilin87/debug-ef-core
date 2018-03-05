5dadabcc632a6ea778c6ed0ceee74e8c4ee662e5
Camilo Sanchez <camilin87@gmail.com>
2018-03-05 14:40:21 -0500

    create the extensions to the DbContextOptionsBuilder

---
+++ DebugEFCore/DbContextOptionsBuilderExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace store_scrapper_2.DAL.Db
+{
+  public static class DbContextOptionsBuilderExtensions
+  {
+    public static void ConfigureLogging(this DbContextOptionsBuilder optionsBuilder, bool loggingEnabled)
+    {
+      if (loggingEnabled)
+      {
+        optionsBuilder.UseLoggerFactory(DataContextLoggerProvider.CreateFactory());
+      }
+    }
+  }
+}
